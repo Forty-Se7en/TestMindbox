eab122b4d63698f92bd7be9d2f3a902db29b616b
Evgeniy <eugenn@internet.ru>
2024-12-11 20:15:45 +0300

    Remove order by p.name

--- query.sql
+++ query.sql
@@ -1,5 +1,4 @@
 ﻿select p.name, c.name
 from products as p
 left join products_categories as pc on p.id = pc.product_id
-left join categories as c on pc.category_id = c.id 
-order by p.name
+left join categories as c on pc.category_id = c.id
